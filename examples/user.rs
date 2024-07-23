16c22ff4e391bf94a2e86dcf4ad49f023abe95b1
Mike P <buhaytza2005@gmail.com>
2024-07-23 11:16:13 +0100

    chore: cleanup examples

--- examples/user.rs
+++
@@ -1,8 +0,0 @@
-use cron_manager::current_user;
-
-fn main() {
-    match current_user() {
-        Some(username) => println!("Current username: {}", username),
-        None => println!("Failed to get the current username"),
-    }
-}
